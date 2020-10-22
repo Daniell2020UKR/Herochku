3776cfa727e048432cd3af2c0badad9db2529cd3
Daniell2020UKR <66268402+Daniell2020UKR@users.noreply.github.com>
2020-10-22 11:35:01 +0300

    Update akira_lang.py

--- akira_lang.py
+++ akira_lang.py
@@ -1,6 +1,6 @@
 translations = {
     'en': {
-        'akira_start': 'Hi! Type \".help\" to get a list of commands.',
+        'akira_start': 'Hi! Type \"/help\" to get a list of commands.',
         'akira_noargs': 'No arguments.',
         'akira_version': 'My version - ',
         'akira_nolang': 'This language is not supported.',
@@ -14,16 +14,16 @@ translations = {
         'akira_audio_upload_error': 'An error occurred while trying to upload audio.',
         'akira_changelog': 'Version 0.1 pre-alpha — SC modded \& bot started firstly. Version 0.2-alpha — SC modded x2 \& added y2v function (PRE-ALPHA), bug fix',
         'akira_help':
-            '.start - Start using the bot.\n'
-            '.donate - Donation info.\n'
-            '.setlang - Set language (only en&ru).\n'
-            '.y2a - Download audio from Youtube\n'
-            '.y2v - Download video from YouTube (PRE-ALPHA!)\n'
-            '.changelog - Changelog and version info\n'
-            '.version - Bot version.'
+            '/start - Start using the bot.\n'
+            '/donate - Donation info.\n'
+            '/setlang - Set language (only en&ru).\n'
+            '/y2a - Download audio from Youtube\n'
+            '/y2v - Download video from YouTube (PRE-ALPHA!)\n'
+            '/changelog - Changelog and version info\n'
+            '/version - Bot version.'
     },
     'ru': {
-        'akira_start': 'Привет! Напишите \".help\" чтобы получить список команд.',
+        'akira_start': 'Привет! Напишите \"/help\" чтобы получить список команд.',
         'akira_noargs': 'Нету агрументов.',
         'akira_version': 'Моя версия - ',
         'akira_nolang': 'Этот язык не поддерживается.',
@@ -33,7 +33,7 @@ translations = {
         'akira_donate': 'Донат разработчику исхов - @Myst33dDonate',
         'akira_downloading': 'Скачиваю...',
         'akira_uploading': 'Загружаю...',
-        'akira_changelog': 'Версия 0.1 pre-alpha — ИК модифицирован, бот запущен впервые. Версия 0.2-alpha — ИК модифицирован x2, добавлена y2v функция (ПРЕ-АЛЬФА), баг фикс', 
+        'akira_changelog': 'Версия 0.1 pre-alpha — ИК модифицирован, бот запущен впервые. Версия 0.2-alpha — ИК модифицирован x2, добавлена y2v функция (ПРЕ-АЛЬФА), баг фикс. Версия 0.3-beta — бот запущен на Heroku, комманды начинаются не с \"<<.>>, а с <</>>\" ', 
         'akira_audio_download_error': 'Произошла ошибка пытаясь скачать аудио.',
         'akira_audio_upload_error': 'Произошла ошибка пытаясь загрузить аудио.',
         'akira_help':
@@ -42,7 +42,7 @@ translations = {
             '.addme - Добавить пользователя в контакты бота.\n'
             '.setlang - Установить язык.\n'
             '.y2a - Скачать аудио с Youtube\n'
-            '.y2v - Скачать видео с YouTube (ПРЕ-АЛЬФА!)\n'
+            '.y2v - Скачать видео с YouTube (АЛЬФА)\n'
             '.changelog - список версий и изменений\n' 
             '.version - Версия бота.'
     }
